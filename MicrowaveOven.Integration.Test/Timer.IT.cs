3aaec45af98b1473db3b7c9a7f59ce1545a46793
Bertram Eriksen <djokkins@live.dk>
2019-12-10 16:59:45 +0100

    rettet fejl i powertube, max power var 100 %, men burde være 700 W. Unit test på powertube fejler nu

--- MicrowaveOven.Integration.Test/Timer.IT.cs
+++ MicrowaveOven.Integration.Test/Timer.IT.cs
@@ -7,6 +7,7 @@
 //using System.Linq;
 //using System.Text;
 //using System.Threading.Tasks;
+//using NSubstitute;
 
 //namespace MicrowaveOven.Integration.Test
 //{
@@ -15,41 +16,27 @@
 //    {
 //        class Timer_IT
 //        {
-//            private IButton powerButton;
-//            private IButton timeButton;
-//            private IButton startCancelButton;
-//            private IDoor door;
-//            private IUserInterface ui;
-//            private ILight light;
 //            private ICookController cooker;
-//            private IOutput output;
+//            private IOutput fake_output;
 //            private ITimer _uut;
-//            private IPowerTube powertube;
-//            private IDisplay display;
-//            private Button button;
+//            private IPowerTube fake_powertube;
+//            private IDisplay fake_display;
 
 //            [SetUp]
 //            public void SetUp()
 //            {
-//                powertube = new PowerTube(output);
-//                powerButton = new Button();
-//                timeButton = new Button();
-//                startCancelButton = new Button();
-//                door = new Door();
-//                output = new Output();
-//                display = new Display(output);
-//                light = new Light(output);
+//                fake_output = Substitute.For<IOutput>();
+//                fake_display = Substitute.For<IDisplay>();
+//                fake_powertube = Substitute.For<IPowerTube>();
 //                _uut = new MicrowaveOvenClasses.Boundary.Timer();
-//                cooker = new CookController(_uut, display, powertube);
-//                ui = new UserInterface(powerButton, timeButton, startCancelButton, door, display, light, cooker);
-//                //evt lav fakes, det gør programmet nemmere at teste. IT test er en "unittest" af controlleren. 
+//                cooker = new CookController(_uut, fake_display, fake_powertube);
 //            }
 //            [Test]
 //            public void timer_activation()
 //            {
 //                int time = 5;
 //                cooker.StartCooking(50, 1);
-//                Assert.That(_uut.TimeRemaining, Is.EqualTo(time));
+//                fake_output.Received().OutputLine($"PowerTube works with {time} %");
 //            }
 
 //            [Test]
